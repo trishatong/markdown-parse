c506fe39a2792ce0ecb15a872bb59d7c946d5b7a
trishatong <trtong1@gmail.com>
2022-01-28 15:15:12 -0800

    initialized variables

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,19 +9,22 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-
-        int currentIndex;
+        int currentIndex = 0;
+        int openParen = 0;
+        int nextOpenBracket = 0;
+        int nextCloseBracket = 0;
+        int closeParen = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            nextOpenBracket = markdown.indexOf("[", currentIndex);
             if(currentIndex > 0 && markdown.indexOf("!", currentIndex) == nextOpenBracket - 1) {
                 currentIndex = nextOpenBracket + 1;
                 continue;
             }
             if(nextOpenBracket >= 0) {
-                int openParen = markdown.indexOf("(", currentIndex);
-                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                openParen = markdown.indexOf("(", currentIndex);
+                nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 if(openParen > 0 && nextCloseBracket == openParen - 1) {
-                    int closeParen = markdown.indexOf(")", openParen);
+                    closeParen = markdown.indexOf(")", openParen);
                     if(closeParen >= 0) {
                         if(markdown.substring(openParen + 1, closeParen).length() > 0) {
                             toReturn.add(markdown.substring(openParen + 1, closeParen));
