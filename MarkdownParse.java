7281485501f7c0747c16c29b0de89e9295beea74
trishatong <trtong1@gmail.com>
2022-01-28 15:14:39 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,19 +9,19 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-        int currentIndex = 0;
-        int openParen = 0;
-        int nextOpenBracket = 0;
-        int nextCloseBracket = 0;
-        int closeParen = 0;
-        while(currentIndex < markdown.length()) {
-            nextOpenBracket = markdown.indexOf("[", currentIndex);
 
+        int currentIndex;
+        while(currentIndex < markdown.length()) {
+            int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(currentIndex > 0 && markdown.indexOf("!", currentIndex) == nextOpenBracket - 1) {
+                currentIndex = nextOpenBracket + 1;
+                continue;
+            }
             if(nextOpenBracket >= 0) {
-                openParen = markdown.indexOf("(", currentIndex);
-                nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", currentIndex);
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 if(openParen > 0 && nextCloseBracket == openParen - 1) {
-                    closeParen = markdown.indexOf(")", openParen);
+                    int closeParen = markdown.indexOf(")", openParen);
                     if(closeParen >= 0) {
                         if(markdown.substring(openParen + 1, closeParen).length() > 0) {
                             toReturn.add(markdown.substring(openParen + 1, closeParen));
