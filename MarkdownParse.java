5beede92ff4c271d98412b2a901510b9a4117b65
trishatong <trtong1@gmail.com>
2022-02-15 13:40:23 -0800

    quiz

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,4 +1,32 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+// // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+// import java.util.ArrayList;
+
+// public class MarkdownParse {
+//     public static ArrayList<String> getLinks(String markdown) {
+//         ArrayList<String> toReturn = new ArrayList<>();
+//         // find the next [, then find the ], then find the (, then take up to
+//         // the next )
+// int currentIndex = 0;
+//         while(currentIndex < markdown.length()) {
+//             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+//             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+//             int openParen = markdown.indexOf("(", nextCloseBracket);
+//             int closeParen = markdown.indexOf(")", openParen);
+//             toReturn.add(markdown.substring(openParen + 1, closeParen));
+//             currentIndex = closeParen + 1;
+//         }
+//         return toReturn;
+//     }
+//     public static void main(String[] args) throws IOException {
+// 		Path fileName = Path.of(args[0]);
+// 	    String contents = Files.readString(fileName);
+//         ArrayList<String> links = getLinks(contents);
+//         System.out.println(links);
+//     }
+// }
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,14 +37,23 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-int currentIndex = 0;
+        int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket == -1) {
+                break;
+            }
+            if(nextOpenBracket > 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
+                break;
+            }
+            int nextCloseBracket = markdown.indexOf("](", nextOpenBracket);
+            if(nextCloseBracket == -1) {
+                break;
+            }
+            int closeParen = markdown.indexOf(")", nextCloseBracket);
+            toReturn.add(markdown.substring(nextCloseBracket + 2, closeParen));
             currentIndex = closeParen + 1;
+            // System.out.format("%d\t%d\t%d\t%d\n", currentIndex, nextOpenBracket, nextCloseBracket, closeParen);
         }
         return toReturn;
     }
